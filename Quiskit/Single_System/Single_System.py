ec432be948138da11108fea639f8d50c21bf8125
patrickkelly3 <p3kellyga@gmail.com>
2025-01-12 23:56:10 -0500

    Matrix Multiplication examples

--- Quiskit/Single_System/Single_System.py
+++ Quiskit/Single_System/Single_System.py
@@ -17,3 +17,13 @@ M1 = np.array([[1, 1], [0,0]])
 M2 = np.array([[1,0], [0,1]])
 M = M1/2 + M2/2
 print(M)
+
+print("\n")
+
+#Matrix multiplication
+print("M1 * ket1: ")
+print(np.matmul(M1, ket1))
+print("M1 * M2:")
+print( np.matmul(M1, M2))
+print("M^2:" )
+print( np.matmul(M,M))
