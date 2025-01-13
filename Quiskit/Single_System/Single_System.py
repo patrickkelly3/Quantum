b9e675ef969406f713c69fe715dfd48d30bb15a4
patrickkelly3 <p3kellyga@gmail.com>
2025-01-12 23:39:32 -0500

    Created matrix representation using array class

--- Quiskit/Single_System/Single_System.py
+++ Quiskit/Single_System/Single_System.py
@@ -10,4 +10,10 @@ ket0 = np.array([[1], [0]])
 ket1 = np.array([[0], [1]])
 print(ket0/2 + ket1/2) #Prints average of the two column vectors
 
+print("\n")
 
+#Matrices
+M1 = np.array([[1, 1], [0,0]])
+M2 = np.array([[1,0], [0,1]])
+M = M1/2 + M2/2
+print(M)
